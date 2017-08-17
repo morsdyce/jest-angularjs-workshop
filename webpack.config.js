e1a8c46e886f7c970d0d9517afbc028a51a6089c
Maayan Glikser <maayan@glikm.com>
2017-08-17 09:53:50 +0300

    Upgrade to webpack 3

--- webpack.config.js
+++ webpack.config.js
@@ -6,27 +6,30 @@ module.exports = {
 
   // enable loading modules relatively (without the ../../ prefix)
   resolve: {
-    root: [__dirname]
+    modules: [
+      __dirname,
+      "node_modules"
+    ]
   },
 
   module: {
-    loaders: [
+    rules: [
 
       // load and compile javascript
-      {test: /\.jsx?$/, exclude: /node_modules/, loader: "babel"},
+      {test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader"},
 
       // load css and process scss
-      {test: /\.css/, loader: "style!css"},
+      {test: /\.css/, loader: "style-loader!css-loader"},
 
       // load JSON files and HTML
       {test: /\.json$/, loader: "json"},
-      {test: /\.html$/, exclude: /node_modules/, loader: "raw"},
+      {test: /\.html$/, exclude: /node_modules/, loader: "raw-loader"},
 
       // load fonts(inline base64 URLs for <=8k)
-      {test: /\.(ttf|eot|svg|otf)$/, loader: "file"},
+      {test: /\.(ttf|eot|svg|otf)$/, loader: "file-loader"},
       {
         test: /\.woff(2)?$/,
-        loader: "url?limit=8192&minetype=application/font-woff"
+        loader: "url-loader?limit=8192&minetype=application/font-woff"
       },
 
       // load images (inline base64 URLs for <=8k images)
@@ -36,8 +39,7 @@ module.exports = {
 
   // webpack dev server configuration
   devServer: {
-    noInfo: false,
-    hot: true
+    noInfo: false
   },
 
   // support source maps
