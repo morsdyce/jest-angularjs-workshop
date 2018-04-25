6a823151d23ef74cadb990772705f3b23d42f1fe
Maayan Glikser <maayan@glikm.com>
2018-04-25 11:58:56 +0300

    Remove react

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 import 'common/assets/stylesheets/global.css';
 
 import angular from 'angular';
-import React from 'react';
-import ReactDOM from 'react-dom';
 
 import AngularMinesweeper from 'angular-minesweeper';
-import ReactMinesweeper from 'react-minesweeper/app';
 
 let angularContainer = document.getElementById('angular');
-let reactContainer   = document.getElementById('react');
 
 // render angular to #angular dom node
 angular.bootstrap(angularContainer, [AngularMinesweeper.name]);
-
-// render react into #react dom node
-ReactDOM.render(<ReactMinesweeper />, reactContainer);
