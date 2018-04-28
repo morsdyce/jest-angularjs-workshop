55fac7ffe3d65fb5dce69c79ddc46daddb1f8e84
Maayan Glikser <maayan@glikm.com>
2018-04-29 01:06:27 +0300

    upgrade to babel-preset-env and add config for build

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,19 @@
-module.exports = {
+const path = require('path');
 
-  // set the context (optional)
+module.exports = {
   context: __dirname,
   entry: 'index.js',
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: "main.js"
+  },
 
   // enable loading modules relatively (without the ../../ prefix)
   resolve: {
     modules: [
-      __dirname,
-      "node_modules"
+      "node_modules",
+      __dirname
     ]
   },
 
@@ -16,32 +21,50 @@ module.exports = {
     rules: [
 
       // load and compile javascript
-      {test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader"},
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
 
       // load css and process scss
-      {test: /\.css/, loader: "style-loader!css-loader"},
+      {
+        test: /\.css/,
+        use: ["style-loader", "css-loader"]
+      },
 
-      // load JSON files and HTML
-      {test: /\.json$/, loader: "json"},
-      {test: /\.html$/, exclude: /node_modules/, loader: "raw-loader"},
+      // load JSON files
+      {
+        test: /\.json$/,
+        loader: "json"
+      },
+
+      // load HTML files
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        loader: "raw-loader"
+      },
 
       // load fonts(inline base64 URLs for <=8k)
-      {test: /\.(ttf|eot|svg|otf)$/, loader: "file-loader"},
       {
-        test: /\.woff(2)?$/,
-        loader: "url-loader?limit=8192&minetype=application/font-woff"
+        test: /\.(ttf|eot|svg|otf|woff)$/,
+        loader: "file-loader"
       },
 
       // load images (inline base64 URLs for <=8k images)
-      {test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'}
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 8192
+        }
+      }
     ]
   },
 
   // webpack dev server configuration
   devServer: {
     noInfo: false
-  },
-
-  // support source maps
-  devtool: "#inline-source-map"
+  }
 };
