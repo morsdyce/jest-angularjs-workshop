06c803b3825922a141a6b3d429fa5978c8c7f8b6
Maayan Glikser <maayan@glikm.com>
2018-04-28 23:36:48 +0300

    Remove react

--- react-minesweeper/components/board/header/header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Header = ({ children }) => (
-  <div className="header">
-    {children}
-  </div>
-);
-
-export default Header;
--- react-minesweeper/constants/game.constants.js
+++
@@ -1,9 +0,0 @@
-export const REVEAL_SPOT = 'REVEAL_SPOT';
-export const FLAG_SPOT = 'FLAG_SPOT';
-export const NEW_GAME = 'NEW_GAME';
-
-export default {
-  REVEAL_SPOT,
-  FLAG_SPOT,
-  NEW_GAME
-}
